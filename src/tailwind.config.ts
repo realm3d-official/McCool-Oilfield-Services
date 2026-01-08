622e89643c3c9ea5e165ca97dbaa32dcb8928438
realm3d-official <info@realm3d.com>
2026-01-07 18:45:51 -0700

    yes

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -1,14 +1,9 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
